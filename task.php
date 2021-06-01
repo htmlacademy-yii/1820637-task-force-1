09500d8aa63549b6e8ae61a877fdb945a1e4a27d
Rustam <dzanni@gmail.com>
2021-06-01 16:54:57 +0300

    Обновили task.php

--- task.php
+++ task.php
@@ -3,28 +3,6 @@
 class Task
 {
 
-// Конструктор с обоими id
-
-public function __construct($id_owner, $id_worker)
-{
-        $this->id_owner = $id_owner;
-        $this->id_worker = $id_worker;
-}
-
-// Переменные статуса и действия
-
-public $status;
-public $action;
-
-
-// Функция возврата статуса
-
-public function getStatus() {
-        return $this->status;
-    }
-
-
-
 // Статусы
 
 const STATUS_NEW = 'Новое';
@@ -47,69 +25,84 @@ const ACTION_APPLY = 'Откликнуться';
 const ACTION_STRIKE = 'Отказаться';
 
 
+// Конструктор
+
+public function __construct($ownerId, $workerId, $status)
+{
+    $this->ownerId = $ownerId;
+    $this->workerId = $workerId;
+    $this->status = $status;
+}
+
+// Функция возврата статуса
+
+public function getStatus()
+{
+    return $this->status;
+}
 
 // Вывод списка возможных статусов и действий
 
-public function listStatus() {
+public function getListStatus()
+{
+    $status = array("STATUS_NEW" => 'Новое',
+    "STATUS_CANCEL" => 'Отменено',
+    "STATUS_WORK" => 'В работе',
+    "STATUS_DEAD" => 'Провалено',
+    "STATUS_DONE" => 'Выполнено');
 
-$status = array("STATUS_NEW" => 'Новое',
-"STATUS_CANCEL" => 'Отменено',
-"STATUS_WORK" => 'В работе',
-"STATUS_DEAD" => 'Провалено',
-"STATUS_DONE" => 'Выполнено');
+    $action = array("ACTION_NEW" => 'Добавить',
+    "ACTION_CANCEL" => 'Отменить',
+    "ACTION_WORK" => 'Принять',
+    "ACTION_DEAD" => 'Заявить о проблеме',
+    "ACTION_DONE" => 'Завершить',
+    "ACTION_APPLY" => 'Откликнуться',
+    "ACTION_STRIKE" => 'Отказаться');
 
-$action = array("ACTION_NEW" => 'Добавить',
-"ACTION_CANCEL" => 'Отменить',
-"ACTION_WORK" => 'Принять',
-"ACTION_DEAD" => 'Заявить о проблеме',
-"ACTION_DONE" => 'Завершить',
-"ACTION_APPLY" => 'Откликнуться';
-"ACTION_STRIKE" => 'Отказаться');
+    echo "Статусы:\n";
+    foreach($status as $key => $value) {
+        echo $key."\t=>\t".$value."\n";
+    }
 
-echo "Статусы\n\n";
-foreach($status as $key => $value){
-    echo $key."\t=>\t".$value."\n";
+    echo "Действия:\n";
+    foreach($action as $key => $value) {
+        echo $key."\t=>\t".$value."\n";
+    }
 }
-
-echo "\nДействия\n\n";
-foreach($action as $key => $value){
-    echo $key."\t=>\t".$value."\n";
-}
-}
-
-
 
 // Вывод на печать правил перехода ("Откликнуться" не меняет статус)
 
-public function getNextStatus() {
+public function getNextStatus()
+{
+    $actionToStatus = array("ACTION_NEW" => 'STATUS_NEW',
+    "ACTION_CANCEL" => 'STATUS_CANCEL',
+    "ACTION_WORK" => 'STATUS_WORK',
+    "ACTION_DEAD" => 'STATUS_DEAD',
+    "ACTION_DONE" => 'STATUS_DONE',
+    "ACTION_APPLY" => 'STATUS_NEW',
+    "ACTION_STRIKE" => 'STATUS_DEAD');
 
-$actionToStatus = array("ACTION_NEW" => 'STATUS_NEW',
-"ACTION_CANCEL" => 'STATUS_CANCEL',
-"ACTION_WORK" => 'STATUS_WORK',
-"ACTION_DEAD" => 'STATUS_DEAD',
-"ACTION_DONE" => 'STATUS_DONE',
-"ACTION_APPLY" => 'STATUS_NEW',
-"ACTION_STRIKE" => 'STATUS_DEAD');
-
-echo "Статус после действия\n\n";
-foreach( $actionToStatus as $key => $value ){
-    echo $key."\t=>\t".$value."\n";
+    echo "Статус после действия:\n";
+    foreach($actionToStatus as $key => $value) {
+        echo $key."\t=>\t".$value."\n";
+    }
 }
-}
-
 
 // Вывод на печать возможных действий в статусе
 
-public function actionInStatus() {
+public function getActionInStatus()
+{
+    if ($this->status == 'STATUS_NEW') {
+        $actionInStatus = array('ACTION_CANCEL', 'ACTION_APPLY', 'ACTION_WORK');
+    } elseif ($this->status == 'STATUS_WORK') {
+        $actionInStatus = array ('ACTION_DONE', 'ACTION_APPLY', 'ACTION_STRIKE');
+    } else $actionInStatus = array ('Нет возможных действий!');
 
-if ($this->status == STATUS_NEW) $actionInStatus = array('ACTION_CANCEL', 'ACTION_APPLY', 'ACTION_WORK');
-elseif ($this->status == STATUS_WORK) $actionInStatus = array ('ACTION_DONE', 'ACTION_APPLY', 'ACTION_STRIKE');
-else $actionInStatus = array ('Нет возможных действий!');
-
-	
-echo "Действия в текущем статусе\n\n";
-foreach ($actionInStatus as $action) echo $action."\n";
-
+    echo "Действия в текущем статусе:\n";
+    foreach ($actionInStatus as $action) {
+        echo $action."\n";
+    }
+}
 }
 
 php>
