9b037c61140461eb30433006b860cf424b45a176
dzanni <dzanni@gmail.com>
2021-06-25 15:33:42 +0300

    Поправил таблицу после второй проверки

--- module2-task1.sql
+++ module2-task1.sql
@@ -17,52 +17,55 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 --
 -- 1820637-task-force-1
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `1820637-task-force-1` DEFAULT CHARACTER SET utf8 ;
-USE `1820637-task-force-1` ;
+CREATE SCHEMA IF NOT EXISTS `1820637_task_force_1` DEFAULT CHARACTER SET utf8 ;
+USE `1820637_task_force_1` ;
 
 -- -----------------------------------------------------
 -- Table `1820637-task-force-1`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`users` (
-  `user_id` INT NOT NULL AUTO_INCREMENT,
-  `date_add` DATE NOT NULL COMMENT 'Таблица пользователей',
+CREATE TABLE IF NOT EXISTS `1820637_task_force_1`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `creation_time` DATETIME NOT NULL,
   `name` VARCHAR(50) NOT NULL,
-  `password` VARCHAR(20) NOT NULL,
-  `e-mail` VARCHAR(40) NOT NULL,
-   PRIMARY KEY (`user_id`))
+  `password` VARCHAR(60) NOT NULL,
+  `email` VARCHAR(40) NOT NULL,
+   PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `1820637-task-force-1`.`profiles`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`profiles` (
+CREATE TABLE IF NOT EXISTS `1820637_task_force_1`.`profiles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL;
   `avatar_file` VARCHAR(20) NULL,
   `download_files` VARCHAR(50) NULL,
-  `date_of_birth` DATETIME NOT NULL,
+  `birthday` DATETIME NOT NULL,
   `address` VARCHAR(100) NULL,
   `city_id` INT NOT NULL,
   `about` VARCHAR(200) NOT NULL,
-  `e-mail` VARCHAR(40) NOT NULL,
+  `email` VARCHAR(40) NOT NULL,
   `phone` INT NULL,
   `skype` VARCHAR(20) NULL,
-  `other_conntact` VARCHAR(20) NULL,
+  `other_contact` VARCHAR(20) NULL,
   `rating` INT NULL,
-  `last-activity` DATETIME NOT NULL,
-  `box_hide_contacts` TINYINT(1) NULL,
-  `box_hide_profile` TINYINT(1) NOT NULL,
-  `box_message` TINYINT(1) NOT NULL,
-  `box_actions` TINYINT(1) NOT NULL,
-  `box_reply` TINYINT(1) NOT NULL)
+  `last_activity` DATETIME NOT NULL,
+  `hide_contacts` TINYINT(1) NULL,
+  `hide_profile` TINYINT(1) NOT NULL,
+  `message` TINYINT(1) NOT NULL,
+  `actions` TINYINT(1) NOT NULL,
+  `reply` TINYINT(1) NOT NULL),
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `1820637-task-force-1`.`tasks`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`tasks` (
-  `task_id` INT NOT NULL AUTO_INCREMENT,
-  `date_add` DATE NOT NULL COMMENT 'Таблица заданий',
+CREATE TABLE IF NOT EXISTS `1820637_task_force_1`.`tasks` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `creation_time` DATETIME NOT NULL,
   `short_description` VARCHAR(50) NOT NULL,
   `description` VARCHAR(100) NOT NULL,
   `category_id` INT NOT NULL,
@@ -74,87 +77,88 @@ CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`tasks` (
   `owner_id` INT NOT NULL,
   `worker_id` INT NULL,
   `task_status` VARCHAR(10) NOT NULL,
-  PRIMARY KEY (`task_id`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `1820637-task-force-1`.`categories`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`categories` (
-  `category-id` INT NOT NULL AUTO_INCREMENT,
-  `category` VARCHAR(20) NOT NULL COMMENT 'Таблица категорий',
+CREATE TABLE IF NOT EXISTS `1820637_task_force_1`.`categories` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `category` VARCHAR(20) NOT NULL,
   `icon_url` VARCHAR(50) NOT NULL,
-  PRIMARY KEY (`category-id`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `1820637-task-force-1`.`cities`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`cities` (
-  `city_id` INT NOT NULL AUTO_INCREMENT,
-  `city` VARCHAR(20) NOT NULL COMMENT 'Таблица городов',
+CREATE TABLE IF NOT EXISTS `1820637_task_force_1`.`cities` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `city` VARCHAR(20) NOT NULL,
   `point` POINT NOT NULL,
-  PRIMARY KEY (`city_id`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `1820637-task-force-1`.`replies`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`replies` (
-  `reply_id` INT NOT NULL AUTO_INCREMENT,
-  `date_add` DATETIME NOT NULL COMMENT 'Таблица откликов на задания',
+CREATE TABLE IF NOT EXISTS `1820637_task_force_1`.`replies` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `creation_time` DATETIME NOT NULL,
   `user_id` INT NOT NULL,
   `task_id` INT NOT NULL,
   `description` VARCHAR(100) NOT NULL,
   `budjet` INT NULL,
-  PRIMARY KEY (`reply_id`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `1820637-task-force-1`.`opinions`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`opinions` (
-  `opinion_id` INT NOT NULL AUTO_INCREMENT,
-  `date_add` DATE NOT NULL COMMENT 'Таблица отзывов',
+CREATE TABLE IF NOT EXISTS `1820637_task_force_1`.`opinions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `creation_time` DATETIME NOT NULL,
   `user_id` INT NOT NULL,
   `task_id` INT NOT NULL,
   `rate` INT NULL,
   `description` VARCHAR(100) NULL,
-  PRIMARY KEY (`opinion_id`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `1820637-task-force-1`.`chat`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`chat` (
-  `chat_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `1820637_task_force_1`.`chat` (
+  `id` INT NOT NULL AUTO_INCREMENT,
   `sender_id` INT NOT NULL COMMENT 'Таблица чата',
   `receiver_id` INT NOT NULL,
   `message` VARCHAR(200) NOT NULL,
-  PRIMARY KEY (`chat_id`))
+  PRIMARY KEY (`id`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
 -- Table `1820637-task-force-1`.`user_categories`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `1820637-task-force-1`.`user_categories` (
+CREATE TABLE IF NOT EXISTS `1820637_task_force_1`.`user_category` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `userID` INT NOT NULL,
-  `categoryID` INT NOT NULL COMMENT 'Таблица связи пользователей и категорий',
-  INDEX `userID` (`userID`),
-  INDEX `categoryID` (`categoryID`),
- CONSTRAINT `FK_user` FOREIGN KEY (`userID`) 
-        REFERENCES `users` (`users_id`) ON DELETE CASCADE,
-    CONSTRAINT `FK_category` FOREIGN KEY (`categoryID`) 
-        REFERENCES `categories` (`category_id`) ON DELETE CASCADE,
-        PRIMARY KEY (`id`),
-        UNIQUE KEY `relation_row_unique` (`ticketID`,`nominationID`))
+  `user_id` INT NOT NULL,
+  `category_id` INT NOT NULL,
+  INDEX `user_id` (`user_id`),
+  INDEX `category_id` (`category_id`),
+ CONSTRAINT `fk_user` FOREIGN KEY (`user_id`)
+        REFERENCES `users` (`id`) ON DELETE CASCADE,
+    CONSTRAINT `fk_category` FOREIGN KEY (`category_id`)
+        REFERENCES `categories` (`id`) ON DELETE CASCADE,
+PRIMARY KEY ('id'),
+UNIQUE KEY `relation_row_unique` (`user_id`, `category_id`)
+)
 ENGINE = InnoDB;
 
 
